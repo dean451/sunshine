a2b5f79dfef4e2573d4e7f61a5cd2cdf82815962
Dean Palm <dean451@gmail.com>
2017-08-03 17:44:11 -0500

    aws s3 bucket watch out

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
-  :bucket => 'sunshine-feels'
+  bucket: ENV.fetch('AWS_S3_BUCKET')
   }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
