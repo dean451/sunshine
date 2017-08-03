a6f15cf280fd1de84cf5ff9e3de00e272f50405f
Dean Palm <dean451@gmail.com>
2017-08-03 17:41:57 -0500

    DONT COMMIT YOUR SECRET KEY TO THE REPO DUMMY

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('sunshine-feels'),
-    access_key_id: ENV.fetch('AKIAIU42GHUQTEHQVHIA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('US West (Oregon)'),
+    bucket: ENV.fetch('AWS_S3_BUCKET'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_S3_REGION'),
     }
   }
 
