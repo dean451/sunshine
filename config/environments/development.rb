73081f672afdc51a87c8fa1c19a08e0440269a97
Dean Palm <dean451@gmail.com>
2017-08-04 11:58:19 -0500

    lots of nice little updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,9 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.paperclip_defaults = {
-  :storage => :s3,
-  bucket: ENV('AWS_S3_BUCKET')
-  }
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
