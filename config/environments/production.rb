3bd381faf023616195f4332ba42a4e9ad228337b
Dean Palm <dean@sxsw.com>
2017-08-05 18:34:18 -0500

    production on herkou

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.paperclip_defaults =
-  storage: :s3,
+  config.paperclip_defaults = {
+  storage: => :s3,
   s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
